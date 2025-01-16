7aabe565d838992f31a3da7d33d043db34b3efd9
maxfang168 <159708634+maxfang168@users.noreply.github.com>
2025-01-16 18:01:59 -0500

    test

--- main.cpp
+++ main.cpp
@@ -1,1 +1,314 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <sstream>
+#include <algorithm>
 
+std::vector<int> memoryInt;
+std::unordered_map<std::string, std::vector<std::string>> functions;
+std::unordered_map<std::string, int> variables;
+bool running = true;
+bool breakLoop = false;
+
+void runCodeLine(std::string line);
+
+void runCodeBlock(std::istringstream &stream)
+{
+    std::string line;
+    while (std::getline(stream, line))
+    {
+        if (breakLoop)
+        {
+            breakLoop = false;
+            break;
+        }
+        runCodeLine(line);
+    }
+}
+
+void runCodeLine(std::string line)
+{
+    switch (line[0])
+    {
+    case 'A': // Add an integer to memory
+        memoryInt.push_back(std::stoi(line.substr(1)));
+        break;
+    case 'P': // Print all integers in memory
+        for (int num : memoryInt)
+        {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+        break;
+    case 'C': // Clear the memory
+        memoryInt.clear();
+        break;
+    case 'S':
+    { // Set a specific index in the memory to a new value
+        size_t spacePos = line.find(' ');
+        int index = std::stoi(line.substr(1, spacePos - 1));
+        int value = std::stoi(line.substr(spacePos + 1));
+        if (index >= 0 && index < memoryInt.size())
+        {
+            memoryInt[index] = value;
+        }
+        else
+        {
+            std::cerr << "Index out of bounds" << std::endl;
+        }
+        break;
+    }
+    case 'G':
+    { // Get the value at a specific index in the memory
+        int index = std::stoi(line.substr(1));
+        if (index >= 0 && index < memoryInt.size())
+        {
+            std::cout << memoryInt[index] << std::endl;
+        }
+        else
+        {
+            std::cerr << "Index out of bounds" << std::endl;
+        }
+        break;
+    }
+    case 'R':
+    { // Remove the value at a specific index in the memory
+        int index = std::stoi(line.substr(1));
+        if (index >= 0 && index < memoryInt.size())
+        {
+            memoryInt.erase(memoryInt.begin() + index);
+        }
+        else
+        {
+            std::cerr << "Index out of bounds" << std::endl;
+        }
+        break;
+    }
+    case 'I':
+    { // Take input from the user and store it in memory
+        int value;
+        std::cin >> value;
+        memoryInt.push_back(value);
+        break;
+    }
+    case 'M':
+    { // Move a value from one index to another
+        size_t spacePos = line.find(' ');
+        int fromIndex = std::stoi(line.substr(1, spacePos - 1));
+        int toIndex = std::stoi(line.substr(spacePos + 1));
+        if (fromIndex >= 0 && fromIndex < memoryInt.size() && toIndex >= 0 && toIndex <= memoryInt.size())
+        {
+            int value = memoryInt[fromIndex];
+            memoryInt.erase(memoryInt.begin() + fromIndex);
+            memoryInt.insert(memoryInt.begin() + toIndex, value);
+        }
+        else
+        {
+            std::cerr << "Index out of bounds" << std::endl;
+        }
+        break;
+    }
+    case 'D':
+    { // Duplicate a value at a specific index
+        int index = std::stoi(line.substr(1));
+        if (index >= 0 && index < memoryInt.size())
+        {
+            memoryInt.insert(memoryInt.begin() + index, memoryInt[index]);
+        }
+        else
+        {
+            std::cerr << "Index out of bounds" << std::endl;
+        }
+        break;
+    }
+    case 'F':
+    { // Define a function
+        size_t spacePos = line.find(' ');
+        std::string functionName = line.substr(1, spacePos - 1);
+        std::string commands = line.substr(spacePos + 1);
+        std::istringstream stream(commands);
+        std::vector<std::string> functionLines;
+        std::string functionLine;
+        while (std::getline(stream, functionLine))
+        {
+            functionLines.push_back(functionLine);
+        }
+        functions[functionName] = functionLines;
+        break;
+    }
+    case 'f':
+    { // Call a function
+        std::string functionName = line.substr(1);
+        if (functions.find(functionName) != functions.end())
+        {
+            for (const std::string &functionLine : functions[functionName])
+            {
+                runCodeLine(functionLine);
+            }
+        }
+        else
+        {
+            std::cerr << "Function not found: " << functionName << std::endl;
+        }
+        break;
+    }
+    case 'V':
+    { // Define a variable
+        size_t spacePos = line.find(' ');
+        std::string varName = line.substr(1, spacePos - 1);
+        int value = std::stoi(line.substr(spacePos + 1));
+        variables[varName] = value;
+        break;
+    }
+    case 'U':
+    { // Update a variable
+        size_t spacePos = line.find(' ');
+        std::string varName = line.substr(1, spacePos - 1);
+        int value = std::stoi(line.substr(spacePos + 1));
+        if (variables.find(varName) != variables.end())
+        {
+            variables[varName] = value;
+        }
+        else
+        {
+            std::cerr << "Variable not found: " << varName << std::endl;
+        }
+        break;
+    }
+    case 'L':
+    { // Loop a set of commands a specified number of times
+        size_t spacePos = line.find(' ');
+        int count = std::stoi(line.substr(1, spacePos - 1));
+        std::string commands = line.substr(spacePos + 1);
+        std::istringstream stream(commands);
+        for (int i = 0; i < count; ++i)
+        {
+            runCodeBlock(stream);
+            stream.clear();
+            stream.seekg(0, std::ios::beg);
+        }
+        break;
+    }
+    case 'X':
+    { // Execute a block of commands conditionally based on a comparison
+        size_t spacePos1 = line.find(' ');
+        size_t spacePos2 = line.find(' ', spacePos1 + 1);
+        int index = std::stoi(line.substr(1, spacePos1 - 1));
+        int value = std::stoi(line.substr(spacePos1 + 1, spacePos2 - spacePos1 - 1));
+        std::string commands = line.substr(spacePos2 + 1);
+        if (index >= 0 && index < memoryInt.size() && memoryInt[index] == value)
+        {
+            std::istringstream stream(commands);
+            runCodeBlock(stream);
+        }
+        break;
+    }
+    case 'J':
+    { // Jump to a specific line in the code
+        int lineNumber = std::stoi(line.substr(1));
+        // Implement jump logic here
+        break;
+    }
+    case 'E':
+    { // Execute a block of commands if a condition is true
+        size_t spacePos = line.find(' ');
+        int condition = std::stoi(line.substr(1, spacePos - 1));
+        std::string commands = line.substr(spacePos + 1);
+        if (condition)
+        {
+            std::istringstream stream(commands);
+            runCodeBlock(stream);
+        }
+        break;
+    }
+    case 'T':
+    { // Terminate the program with a specific exit code
+        int exitCode = std::stoi(line.substr(1));
+        exit(exitCode);
+        break;
+    }
+    case 'N':
+    { // Negate a value at a specific index
+        int index = std::stoi(line.substr(1));
+        if (index >= 0 && index < memoryInt.size())
+        {
+            memoryInt[index] = -memoryInt[index];
+        }
+        else
+        {
+            std::cerr << "Index out of bounds" << std::endl;
+        }
+        break;
+    }
+    case 'O':
+    { // Output a specific message
+        std::string message = line.substr(1);
+        std::cout << message << std::endl;
+        break;
+    }
+    case 'B': // Break out of a loop
+        breakLoop = true;
+        break;
+    case 'W':
+    { // Execute a block of commands while a condition is true
+        size_t spacePos = line.find(' ');
+        int condition = std::stoi(line.substr(1, spacePos - 1));
+        std::string commands = line.substr(spacePos + 1);
+        std::istringstream stream(commands);
+        while (condition)
+        {
+            runCodeBlock(stream);
+            stream.clear();
+            stream.seekg(0, std::ios::beg);
+            // Update condition if necessary
+        }
+        break;
+    }
+    case 'K':
+    { // Check if a variable exists
+        std::string varName = line.substr(1);
+        if (variables.find(varName) != variables.end())
+        {
+            std::cout << "Variable exists: " << varName << std::endl;
+        }
+        else
+        {
+            std::cerr << "Variable not found: " << varName << std::endl;
+        }
+        break;
+    }
+    case 'Y':
+    { // Execute a block of commands if a variable exists
+        size_t spacePos = line.find(' ');
+        std::string varName = line.substr(1, spacePos - 1);
+        std::string commands = line.substr(spacePos + 1);
+        if (variables.find(varName) != variables.end())
+        {
+            std::istringstream stream(commands);
+            runCodeBlock(stream);
+        }
+        break;
+    }
+    case 'H': // Halt the program
+        running = false;
+        break;
+    case 'Q': // Quit the program
+        running = false;
+        break;
+    default:
+        std::cerr << "Unknown command: " << line[0] << std::endl;
+        break;
+    }
+}
+
+int main()
+{
+    std::string line;
+    while (running)
+    {
+        std::getline(std::cin, line);
+        runCodeLine(line);
+    }
+    return 0;
+}
